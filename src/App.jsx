dd341263f02b4832637ce0c2f33113659ab8ac22
Mauricio Antero <mjunior447@gmail.com>
2025-01-03 18:01:31 -0300

    feat: add basic assets; tailwind configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 const App = () => {
   return (
     <main>
-      <h1 className="text-5xl text-orange-500 font-bold">
+      <h1 className="text-5xl text-violet-300 font-bold">
         Welcome to Zentry Clone
       </h1>
     </main>
